6603da5e2c0e0b7d38cd49b69969f38fbbf52616
Nikolay Petrov <78559158+niki03petrov@users.noreply.github.com>
2026-04-02 17:16:41 +0300

    minor change

--- src/main/java/sofia/sap/interview/project/game/dto/data/GameData.java
+++ src/main/java/sofia/sap/interview/project/game/dto/data/GameData.java
@@ -1,4 +1,4 @@
 package sofia.sap.interview.project.game.dto.data;
 
-public record GameData(sofia.sap.interview.project.game.dto.data.CharacterData character, MapData map, QuestsData quests) {
+public record GameData(CharacterData character, MapData map, QuestsData quests) {
 }
