8b2a0173924a856f7ebc9f49cd4b952a6cee6eb5
Nikolay Petrov <78559158+niki03petrov@users.noreply.github.com>
2026-04-06 10:41:51 +0300

    new game path format change

--- src/main/java/sofia/sap/interview/project/game/files/NewGame.java
+++ src/main/java/sofia/sap/interview/project/game/files/NewGame.java
@@ -1,20 +1,21 @@
 package sofia.sap.interview.project.game.files;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import sofia.sap.interview.project.game.dto.loadgame.PlaygroundFactory;
 import sofia.sap.interview.project.game.dto.data.PlaygroundData;
+import sofia.sap.interview.project.game.dto.loadgame.PlaygroundFactory;
 import sofia.sap.interview.project.game.exceptions.NewGameFileException;
 import sofia.sap.interview.project.game.map.Playground;
 
-import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
 
 public class NewGame {
     public static Playground createPlayground() {
         ObjectMapper mapper = new ObjectMapper();
+        Path mapPath = Path.of("files", "maps", "CommonMap.json");
 
         try {
-            PlaygroundData dto = mapper.readValue(new File("files\\maps\\CommonMap.json"), PlaygroundData.class);
+            PlaygroundData dto = mapper.readValue(mapPath.toFile(), PlaygroundData.class);
             return PlaygroundFactory.create(dto);
         } catch (IOException e) {
             throw new NewGameFileException("Failed to load playground!", e);
